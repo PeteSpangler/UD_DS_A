f823003b7a65934bf218f056eab059048d7c30e4
Pete Spangler <63021506+PeteSpangler@users.noreply.github.com>
2020-06-08 18:50:43 -0700

    finished up trees section?

--- P1/Trees/Breadth.First.search.py
+++ P1/Trees/Breadth.First.search.py
@@ -1,3 +1,7 @@
+"""
+This is using a QUEUE as oppose to a STACK that was used in Depth First
+Search. Cool cool cool
+"""
 class Node(object):
         
     def __init__(self,value = None):
@@ -48,3 +52,29 @@ tree = Tree("apple")
 tree.get_root().set_left_child(Node("banana"))
 tree.get_root().set_right_child(Node("cherry"))
 tree.get_root().get_left_child().set_left_child(Node("dates"))
+
+from collections import deque
+class Queue():
+    def __init__(self):
+        self.q = deque()
+        
+    def enq(self,value):
+        self.q.appendleft(value)
+        
+    def deq(self):
+        if len(self.q) > 0:
+            return self.q.pop()
+        else:
+            return None
+    
+    def __len__(self):
+        return len(self.q)
+    
+    def __repr__(self):
+        if len(self.q) > 0:
+            s = "<enqueue here>\n_________________\n" 
+            s += "\n_________________\n".join([str(item) for item in self.q])
+            s += "\n_________________\n<dequeue here>"
+            return s
+        else:
+            return "<queue is empty>"
