52dd7e3e9a1ee0b9cc08c8c1668fb18b0ecd137c
Pete Spangler <63021506+PeteSpangler@users.noreply.github.com>
2020-06-11 07:57:00 -0700

    cleaned up files

--- P1/Show.Me.The.Data.Structures/problem_6.py
+++ P1/Show.Me.The.Data.Structures/problem_6.py
@@ -1,8 +1,11 @@
 """
 Union and Intersection of Two Linked Lists
-Your task for this problem is to fill out the union and intersection functions. The union of two sets A and B is the set of elements which are in A, in B, or in both A and B. The intersection of two sets A and B, denoted by A ∩ B, is the set of all objects that are members of both the sets A and B.
+Your task for this problem is to fill out the union and intersection functions. The union of two sets A and B is the set of elements 
+which are in A, in B, or in both A and B. The intersection of two sets A and B, denoted by A ∩ B, is the set of all objects that are 
+members of both the sets A and B.
 
-You will take in two linked lists and return a linked list that is composed of either the union or intersection, respectively. Once you have completed the problem you will create your own test cases and perform your own run time analysis on the code.
+You will take in two linked lists and return a linked list that is composed of either the union or intersection, respectively. 
+Once you have completed the problem you will create your own test cases and perform your own run time analysis on the code.
 """
 
 class Node:
