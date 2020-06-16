59d04119b3fa34478d6fc05094aba7fad73336c8
Pete Spangler <63021506+PeteSpangler@users.noreply.github.com>
2020-06-15 21:48:42 -0700

    added problem statements

---
+++ P1/Show.Me.The.Data.Structures/problem_2.py
@@ -0,0 +1,22 @@
+"""
+Finding Files
+For this problem, the goal is to write code for finding all files 
+under a directory (and all directories beneath it) that end with ".c"
+"""
+def find_files(suffix, path):
+    """
+    Find all files beneath path with file name suffix.
+
+    Note that a path may contain further subdirectories
+    and those subdirectories may also contain further subdirectories.
+
+    There are no limit to the depth of the subdirectories can be.
+
+    Args:
+      suffix(str): suffix if the file name to be found
+      path(str): path of the file system
+
+    Returns:
+       a list of paths
+    """
+    return None
