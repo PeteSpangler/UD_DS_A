d8923a38f5bede5201ebe0d2418c37af9f2f8f9c
Pete Spangler <63021506+PeteSpangler@users.noreply.github.com>
2020-06-20 17:10:21 -0700

    Active Directory Solved

--- P1/Show.Me.The.Data.Structures/problem_4.py
+++ P1/Show.Me.The.Data.Structures/problem_4.py
@@ -53,13 +53,30 @@ def is_user_in_group(user, group):
       user(str): user name/id
       group(class:Group): group to check user membership against
     """
-    assert(user)
-    assert(group)
-    if user in group.users:
+    if user in group.get_users():
         return True
-    if len(group.groups) == 0:
-        return False
-    for subgroup in group.groups:
-        return is_user_in_group(user, subgroup)
+    else:
+        if len(group.get_groups()) == 0:
+            return False
+        else:
+            for sub_group in group.get_groups():
+                found = is_user_in_group(group, sub_group)
 
-    return None
+                if found:
+                    return True
+    return False
+
+print('Normal Cases:')
+print(is_user_in_group(user='parent_user', group=parent))
+# False
+print(is_user_in_group(user='child_user', group=parent))
+# False
+print(is_user_in_group(user='sub_child_user', group=parent), '\n')
+# True
+
+# Edge Cases:
+print('Edge Cases:')
+print(is_user_in_group(user='', group=parent))
+# False
+print(is_user_in_group(user='', group=child))
+# False
