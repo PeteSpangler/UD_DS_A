7590b2d385d12e557b106ea4fb158fb6d63fa59a
Pete Spangler <63021506+PeteSpangler@users.noreply.github.com>
2020-07-06 16:57:51 -0700

    basic sorting algos

--- P2/Sorting_Algorithms/bubblesort.py
+++ P2/Sorting_Algorithms/bubblesort.py
@@ -9,3 +9,15 @@ def bubble_sort_1(l):
 
             l[index] = prev
             l[index - 1] = this
+
+def bubble_sort_2(l):
+    for iteration in range(len(l)):
+        for index in range(1, len(l)):
+            this_hour, this_min = l[index]
+            prev_hour, prev_min = l[index - 1]
+
+            if prev_hour > this_hour or (prev_hour == this_hour and prev_min > this_min):
+                continue
+
+            l[index] = (prev_hour, prev_min)
+            l[index - 1] = (this_hour, this_min)
