6c1e15331aca0e29273f2c1cf9c2df60237c4448
Pete Spangler <63021506+PeteSpangler@users.noreply.github.com>
2020-07-15 08:03:53 -0700

    binary search

--- P2/Problems_vs_Algorithms/p1.py
+++ P2/Problems_vs_Algorithms/p1.py
@@ -8,7 +8,7 @@ If the given number is 27, the floor_sqrtwer would be 5 because sqrt(5) = 5.196 
 
 The expected time complexity is O(log(n))
 
-Here is some boilerplate code and test cases to start with:
+Binary Search fufills the requirements:
 """
 def sqrt(x):
   
@@ -30,7 +30,12 @@ def sqrt(x):
    return floor_sqrt
 
 print ("Pass" if  (3 == sqrt(9)) else "Fail")
+#Edge Cases
 print ("Pass" if  (0 == sqrt(0)) else "Fail")
 print ("Pass" if  (4 == sqrt(16)) else "Fail")
 print ("Pass" if  (1 == sqrt(1)) else "Fail")
 print ("Pass" if  (5 == sqrt(27)) else "Fail")
+#Larger Test Cases
+print ("Pass" if  (10 == sqrt(100)) else "Fail")
+print ("Pass" if  (25 == sqrt(627)) else "Fail")
+print ("Pass" if  (5000 == sqrt(25000007)) else "Fail")
