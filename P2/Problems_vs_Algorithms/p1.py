0bf96f90d77968f62b10f60eb06b1a4a7d314b46
Pete Spangler <63021506+PeteSpangler@users.noreply.github.com>
2020-07-15 08:00:28 -0700

    binary search

--- P2/Problems_vs_Algorithms/p1.py
+++ P2/Problems_vs_Algorithms/p1.py
@@ -2,24 +2,32 @@
 Finding the Square Root of an Integer
 Find the square root of the integer without using any Python library. You have to find the floor value of the square root.
 
-For example if the given number is 16, then the answer would be 4.
+For example if the given number is 16, then the floor_sqrtwer would be 4.
 
-If the given number is 27, the answer would be 5 because sqrt(5) = 5.196 whose floor value is 5.
+If the given number is 27, the floor_sqrtwer would be 5 because sqrt(5) = 5.196 whose floor value is 5.
 
 The expected time complexity is O(log(n))
 
 Here is some boilerplate code and test cases to start with:
 """
-def sqrt(number):
-    """
-    Calculate the floored square root of a number
-
-    Args:
-       number(int): Number to find the floored squared root
-    Returns:
-       int: Floored Square Root
-    """
-    pass
+def sqrt(x):
+  
+   if (x == 0 or x ==1):
+      
+      return x
+   
+   low = 1
+   high = x
+   while (low <= high):
+      mid_num = (low + high) // 2
+      if (mid_num*mid_num == x):
+         return mid_num
+      if (mid_num * mid_num < x):
+         low = mid_num + 1
+         floor_sqrt = mid_num
+      else:
+         high = mid_num - 1
+   return floor_sqrt
 
 print ("Pass" if  (3 == sqrt(9)) else "Fail")
 print ("Pass" if  (0 == sqrt(0)) else "Fail")
