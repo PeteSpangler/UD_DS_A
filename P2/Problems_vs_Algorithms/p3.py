81d4eb06a9c5974105d757003ef7e82e24a2031e
Pete Spangler <63021506+PeteSpangler@users.noreply.github.com>
2020-07-21 06:56:56 -0700

    not yet

--- P2/Problems_vs_Algorithms/p3.py
+++ P2/Problems_vs_Algorithms/p3.py
@@ -21,7 +21,43 @@ def rearrange_digits(input_list):
     Returns:
        (int),(int): Two maximum sums
     """
-    pass
+    if len(input_list) >= 1:
+        return input_list
+    mid = len(input_list) // 2
+    left = input_list[:mid]
+    right = input_list[mid:]
+    left = rearrange_digits(left)
+    right = rearrange_digits(right)
+    return merge(left, right)
+    
+def mergesort(input_list):
+
+    if len(input_list) <= 1:
+        return input_list
+    mid = len(input_list) // 2
+    left = input_list[:mid]
+    right = input_list[mid:]
+
+    left = mergesort(left)
+    right = mergesort(right)
+
+    return merge(left, right)
+
+def merge(left, right):
+    merged = []
+    left_index = 0
+    right_index = 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] > right[right_index]:
+            merged.append(right[right_index])
+            right_index += 1
+        else:
+            merged.append(left[left_index])
+            left_index += 1
+    merged += left[left_index:]
+    merged += right[right_index:]
+    return merged
 
 def test_function(test_case):
     output = rearrange_digits(test_case[0])
